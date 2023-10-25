6dca58c2159437a0038d378334b6bd9638c432d9
Dani-Ps <daniperez439@gmail.com>
2023-10-25 14:08:59 +0200

    3

--- 1ºTrimestre/Tareas/UD.3/U3T2_dps/U3T2_dps.js
+++ 1ºTrimestre/Tareas/UD.3/U3T2_dps/U3T2_dps.js
@@ -36,125 +36,20 @@ const DNI_NOMBRE = "Introduce el dni o el nombre completo de socio que deseas ve
 // Para método de mostrar socios por categoria
 const SOCIOS_CATEGORIA = "Introduce la categoría de la que quieras ver los socios:";
 
+// Primera fila con cada conlumna sin categoria 
+const TABLA_1 = "<table><thead><tr><th>Número de Socio</th><th>DNI</th><th>Nombre</th><th>Apellido</th><th>Fecha de Nacimiento</th><th>Localidad</th></tr></thead><tbody>";
+// Primera fila con cada conlumna con categoria 
+const TABLA_2 = "<table><thead><tr><th>Número de Socio</th><th>DNI</th><th>Nombre</th><th>Apellido</th><th>Fecha de Nacimiento</th><th>Localidad</th><th>Categoria</th></tr></thead><tbody>";
+// Almacena el final de la tabla
+const FIN_TABLA = "</tbody></table>";
 // VARIABLES
 
 let socios = []; // Este array almacenará los objetos de tipo Socio.
 let respuesta = ""; // Alamcena la respuesta del usuario según la opción que desee.
 let cancelar = false; // Bandera que hace que el programa se ejecute infinitamente hasta que su valor es = 'true'.
 let socio = new Socio(); // Inicializo el objeto de socio para traer todos los métodos.
+let continuar = ""; // Alamcena el valor que determina si acaba el programa o continua. 
 
-// LOGICA 
-
-while (!cancelar) {
-
-  respuesta = parseInt(prompt("Pulsa \n:" + OPC1 + "\n" + OPC2 + "\n" + OPC3 + "\n" + OPC4 + "\n" + OPC5 + "\n" + OPC6 + "\n" + OPC7 + "\n" + OPC8));
-  switch (respuesta) {
-
-    case 1:
-
-      let dni = prompt(DNI);
-      let nombre = prompt(NOMBRE);
-      let apellido = prompt(APELLIDO);
-      let fechaNacimiento = prompt(FECHA_NACIMIENTO);
-      let localidad = prompt(LOCALIDAD);
-
-      if (socio.alta(dni, nombre, apellido, fechaNacimiento, localidad)) {
-
-        document.write(
-          EXITO_ALTA
-        );
-
-      } else {
-        document.write(
-          ERROR
-        );
-      }
-      break;
-
-    case 2:
-
-      let dato = prompt(NUMS_DNI);
-
-      if (socio.baja(dato)) {
-        document.write(EXITO_BAJA);
-      } else {
-        document.write(ERROR);
-      }
-      break;
-
-    case 3:
-
-      let dato3 = prompt(NUMS_DNI);
-      let localidad3 = prompt(NEW_LOCALIDAD);
-
-      if (socio.modificarLocalidad(dato3, localidad3)) {
-        document.write(EXITO_MODF_LOCL);
-      } else {
-        document.write(ERROR);
-      }
-      break;
-
-    case 4:
-      let fecha = prompt(FECHA_NACIMIENTO);
-      let respuesta = socio.categoria(fecha);
-      if (respuesta != null && respuesta != "") {
-        document.write("<p>" + mensaje + "</p>");
-      } else {
-        document.write(ERROR);
-      }
-      break;
-    case 5:
-
-      let lista5 = socio.socios;
-
-      if (lista5.length > 1) {
-        // hacer tabla
-      } else {
-        document.write(ERROR);
-      }
-      break;
-
-    case 6:
-
-      let dato6 = prompt(DNI_NOMBRE);
-      let socio6 = (socio.mostrarDatos(dato6));
-
-      if (socio6.length > 1) {
-        // hacer tabla
-      } else {
-        document.write(ERROR);
-      }
-
-      break;
-
-    case 7:
-      
-      let categoria = prompt(SOCIOS_CATEGORIA);
-      let lista7 = socio.sociosCategoria(categoria);
-      if (lista7.length > 1) {
-        // hacer tabla
-      } else {
-        document.write(ERROR);
-      }
-      break;
-
-    case 8:
-
-      let localiad = prompt(LOCALIDAD);
-      let lista8 = socio.sociosLocalidad(localiad);
-      if (lista8.length > 1) {
-        // hacer tabla
-      } else {
-        document.write(ERROR);
-      }
-      break;
-    default:
-      document.write(ERROR);
-      break;
-  }
-
-
-}
 
 /**
  *
@@ -215,15 +110,15 @@ function Socio() {
 
   this.baja = function (dato) {
     let exito = false;
-  
+
     socios = socios.filter(function (socio) {
       return socio.numeroSocio !== dato && socio.dni !== dato;
     });
-  
+
     if (socios.length < dimensionLista) {
       exito = true;
     }
-  
+
     return exito;
   };
 
@@ -266,16 +161,21 @@ function Socio() {
   };
 
   this.buscar = function (dato) {
-    let existeSocio = null;
+
+    let existeSocio = new Socio();
+
     socios.forEach(function (socio) {
+
       if (socio.numeroSocio === dato || socio.dni === dato)
         existeSocio = socio;
+
     });
 
     return existeSocio;
   };
 
   this.categoria = function (fechaNacimiento) {
+
     let anho = parseInt(fechaNacimiento.slice(6, 10));
     let edad = 2023 - anho;
     let mensaje = "";
@@ -300,7 +200,9 @@ function Socio() {
   };
 
   this.mostrarDatos = function (dato) {
+
     let socioEncontrado = null;
+
     socios.forEach(function (socio) {
       if (
         socio.dni === dato ||
@@ -308,6 +210,7 @@ function Socio() {
       ) {
         socioEncontrado = socio;
       }
+
     });
 
     return socioEncontrado;
@@ -318,60 +221,249 @@ function Socio() {
     let anhoFiltro;
 
     switch (categoria.toLowerCase()) {
+
       case "senior":
         alert("Senior: Nacidos en 2004 o antes.");
         anhoFiltro = 2004;
         break;
+
       case "juvenil":
         alert("Juvenil: Nacidos en 2005.");
         anhoFiltro = 2005;
         break;
+
       case "cadetes":
         alert("Cadetes: Nacidos en 2006");
         anhoFiltro = 2006;
         break;
+
       case "infantil":
         alert("Infantil: Nacidos en 2007.");
         anhoFiltro = 2007;
         break;
+
       case "alevin":
         alert("Alevín: Nacidos en 2008.");
         anhoFiltro = 2008;
         break;
+
       case "benjamin":
         alert("Benjamín: Nacidos en 2009.");
         anhoFiltro = 2009;
         break;
+
       default:
         alert("Valor no encontrado.");
         break;
     }
 
     socios.forEach(function (socio) {
+
       const fechaNacimiento = socio.fechaNacimiento;
       const anioNacimiento = parseInt(fechaNacimiento.split("/")[2]);
 
-      if (anioNacimiento === anhoFiltro) {
-        lista.push(socio);
-      }
+      if (anioNacimiento === anhoFiltro) lista.push(socio);
+
     });
 
     return lista;
   };
 
   this.sociosLocalidad = function (localidad) {
+
     let lista = [];
 
     socios.forEach(function (socio) {
-      if (socio.localidad === localidad) {
-        lista.push(socio);
-      } else {
-        alert("ERROR. -No hay ningún socio de esa localidad.");
-      }
+
+      if (socio.localidad === localidad) lista.push(socio);
+
     });
 
     return lista;
   };
 }
+
+// LOGICA 
+
+while (!cancelar) {
+
+  respuesta = parseInt(prompt("Pulsa: \n " + OPC1 + "\n" + OPC2 + "\n" + OPC3 + "\n" + OPC4 + "\n" + OPC5 + "\n" + OPC6 + "\n" + OPC7 + "\n" + OPC8));
+
+
+  switch (respuesta) {
+
+    // ALTA
+    case 1:
+      // Pregunto por los datos del nuevo socio
+      let dni = prompt(DNI);
+      let nombre = prompt(NOMBRE);
+      let apellido = prompt(APELLIDO);
+      let fechaNacimiento = prompt(FECHA_NACIMIENTO);
+      let localidad = prompt(LOCALIDAD);
+      // Doy de alta y compruebo si se ha añadido correctsamente, es asi manda un mensaje de exito, en caso contrario manda un mensaje de error.
+      if (socio.alta(dni, nombre, apellido, fechaNacimiento, localidad)) {
+
+        document.write(
+          EXITO_ALTA
+        );
+
+      } else {
+        document.write(
+          ERROR
+        );
+      }
+      break;
+    // BAJA
+    case 2:
+
+      let dato = prompt(NUMS_DNI); // Almacena la variable que identifica al socio.
+
+      if (socio.baja(dato)) { // Ejecuto el metodo de dar de baja, y compruebo si se ha eliminado.
+        document.write(EXITO_BAJA); // Si se ha eliminado manda un mensaje de exito, del contrario manda un mensaje de error.
+      } else {
+        document.write(ERROR);
+      }
+      break;
+
+    // MODIFICAR LOCALIDAD
+    case 3:
+
+      let dato3 = prompt(NUMS_DNI); // Almacena la variable que identifica al socio. 
+      let localidad3 = prompt(NEW_LOCALIDAD); // Almacena la nueva localidad.
+
+      if (socio.modificarLocalidad(dato3, localidad3)) { // Ejecuto el método de cambio de localidad y compruebo si el cambio se ha hecho.
+        document.write(EXITO_MODF_LOCL); // Si se ha realizado correctamente, me salta un mensaje de exito sino uno de error.
+      } else {
+        document.write(ERROR);
+      }
+      break;
+
+    // VER CATEGORIA
+    case 4:
+
+      let fecha = prompt(FECHA_NACIMIENTO);
+      let respuesta = socio.categoria(fecha);
+
+      if (respuesta != null && respuesta != "") {
+        document.write("<p>" + mensaje + "</p>");
+      } else {
+        document.write(ERROR);
+      }
+      break;
+
+    case 5:
+
+      let lista5 = socio.socios;
+
+      if (lista5.length > 1) {
+
+        document.write(TABLA_2);
+ 
+
+        for (let i = 0; i < lista5.length; i++) {
+          document.write("<tr>");
+          document.write("<td>" + lista5[i].numeroSocio + "</td>");
+          document.write("<td>" + lista5[i].dni + "</td>");
+          document.write("<td>" + lista5[i].nombre + "</td>");
+          document.write("<td>" + lista5[i].apellido + "</td>");
+          document.write("<td>" + lista5[i].fechaNacimiento + "</td>");
+          document.write("<td>" + lista5[i].localidad + "</td>");
+          let categoria = obtenerCategoria(lista5[i].fechaNacimiento);
+          document.write("<td>" + categoria + "</td>");
+          document.write("</tr>");
+        }
+
+        document.write(FIN_TABLA);
+   
+        cancelar = true;
+      } else {
+        document.write(ERROR);
+      }
+      break;
+
+    case 6:
+
+      let dato6 = prompt(DNI_NOMBRE);
+      let socio6 = (socio.mostrarDatos(dato6));
+
+      if (socio6.length > 1) {
+
+        document.write(TABLA_1);
+
+        for (let i = 0; i < socio6.length; i++) {
+          document.write("<tr>");
+          document.write("<td>" + socio6[i].numeroSocio + "</td>");
+          document.write("<td>" + socio6[i].dni + "</td>");
+          document.write("<td>" + socio6[i].nombre + "</td>");
+          document.write("<td>" + socio6[i].apellido + "</td>");
+          document.write("<td>" + socio6[i].fechaNacimiento + "</td>");
+          document.write("<td>" + socio6[i].localidad + "</td>");
+          document.write("</tr>");
+        }
+
+        document.write(FIN_TABLA);
+        cancelar = true;
+      } else {
+        document.write(ERROR);
+      }
+
+      break;
+
+    case 7:
+
+      let categoria = prompt(SOCIOS_CATEGORIA);
+      let lista7 = socio.sociosCategoria(categoria);
+      if (lista7.length > 1) {
+        document.write(TABLA_1);
+     
+
+        for (let i = 0; i < lista7.length; i++) {
+          document.write("<tr>");
+          document.write("<td>" + lista7[i].numeroSocio + "</td>");
+          document.write("<td>" + lista7[i].dni + "</td>");
+          document.write("<td>" + lista7[i].nombre + "</td>");
+          document.write("<td>" + lista7[i].apellido + "</td>");
+          document.write("<td>" + lista7[i].fechaNacimiento + "</td>");
+          document.write("<td>" + lista7[i].localidad + "</td>");
+          document.write("</tr>");
+        }
+
+        document.write(FIN_TABLA);
+        cancelar = true;
+      } else {
+        document.write(ERROR);
+      }
+      break;
+
+    case 8:
+
+      let localiad = prompt(LOCALIDAD);
+      let lista8 = socio.sociosLocalidad(localiad);
+      if (lista8.length > 1) {
+        document.write(TABLA_1);
+        for (let i = 0; i < lista.length; i++) {
+          document.write("<tr>");
+          document.write("<td>" + lista8[i].numeroSocio + "</td>");
+          document.write("<td>" + lista8[i].dni + "</td>");
+          document.write("<td>" + lista8[i].nombre + "</td>");
+          document.write("<td>" + lista8[i].apellido + "</td>");
+          document.write("<td>" + lista8[i].fechaNacimiento + "</td>");
+          document.write("<td>" + lista8[i].localiad + "</td>");
+          document.write("</tr>");
+        }
+
+        document.write(FIN_TABLA);
+        cancelar = true;
+      } else {
+        document.write(ERROR);
+      }
+      break;
+    default:
+      document.write(ERROR);
+      break;
+  }
+  
+}
+
 
 

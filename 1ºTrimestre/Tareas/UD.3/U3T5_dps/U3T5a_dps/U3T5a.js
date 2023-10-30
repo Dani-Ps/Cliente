ab00f439f235144260e2629a72e625016c4a3c90
Daniel Perez Serrano <daniperez439@gmail.com>
2023-10-30 19:53:49 +0100

    bingo

--- 1ºTrimestre/Tareas/UD.3/U3T5_dps/U3T5a_dps/U3T5a.js
+++ 1ºTrimestre/Tareas/UD.3/U3T5_dps/U3T5a_dps/U3T5a.js
@@ -1,37 +1,71 @@
-function bingo() {
+let carton = [];
+let numSalidos = [];
 
-    function comenzar() {
+function comenzar() {
+  rellenarCarton();
+  sacaNumeroNuevo();
+}
 
+function rellenarCarton() {
+  for (let index = 0; index < 3; index++) {
+    let fila = [];
 
+    while (fila.length < 5) {
+      let numero = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
 
+      if (!fila.includes(numero)) {
+        fila.push(numero);
+      }
     }
 
-    function rellenarCarton() {
+    carton.push(fila);
+  }
 
-        let carton = [[5], [5], [5]];
+  return carton;
+}
 
-        for (let index = 0; index < 3; index++) {
+function comprobarLinea(carton, numerosSalidos) {
+  let resultado;
 
-            let fila = [];
-
-            while (fila.length < 5) {
-
-                let numero = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
-
-                if (!fila.includes(numero)) {
-                    fila.push(numero);
-                }
-            }
-
-            carton.push(fila);
-        }
-
-        return carton;
-
+  for (let i = 0; i < carton.length; i++) {
+    let lineaCompleta = true;
+    for (let j = 0; j < carton[i].length; j++) {
+      if (!numerosSalidos.includes([i][j])) {
+        lineaCompleta = false;
+        resultado = -1;
+        break;
+      }
     }
-
-    function sacaNumeroNuevo(numeros) {
-        let nuevoNumero
-
+    if (lineaCompleta) {
+      resultado = i + 1;
     }
+  }
+  return resultado;
+}
+
+function comprobarBingo(carton, numerosSalidos) {
+  let resultado = false;
+
+  for (let i = 0; i < carton.length; i++) {
+    let lineaCompleta = true;
+    for (let j = 0; j < carton[i].length; j++) {
+      if (!numerosSalidos.includes([i][j])) {
+        break;
+      } else {
+        resultado = true;
+      }
+    }
+  }
+  return resultado;
+}
+
+function sacaNumeroNuevo(numeros) {
+  if (numSalidos < 99) {
+    let newNumero;
+    do {
+      newNumero = Math.floor(Math.random() * 99) + 1;
+    } while (numSalidos.includes(newNumero));
+    numSalidos.push(newNumero);
+  } else {
+  }
 }

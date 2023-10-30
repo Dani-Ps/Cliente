20f847f1950e562105f4a15ca09ae80290796785
Daniel Perez Serrano <daniperez439@gmail.com>
2023-10-30 21:11:11 +0100

    bingo complete

--- 1ºTrimestre/Tareas/UD.3/U3T5_dps/U3T5a_dps/U3T5a.js
+++ 1ºTrimestre/Tareas/UD.3/U3T5_dps/U3T5a_dps/U3T5a.js
@@ -3,15 +3,18 @@ let numSalidos = [];
 
 function comenzar() {
   rellenarCarton();
-  sacaNumeroNuevo();
+  while (!comprobarBingo(carton, numSalidos) && numSalidos.length < 99) {
+    sacaNumeroNuevo();
+  }
 }
 
 function rellenarCarton() {
+  carton = [];
   for (let index = 0; index < 3; index++) {
     let fila = [];
 
     while (fila.length < 5) {
-      let numero = Math.floor(Math.random() * (99 - 1 + 1)) + 1;
+      let numero = Math.floor(Math.random() * 99) + 1;
 
       if (!fila.includes(numero)) {
         fila.push(numero);
@@ -21,18 +24,17 @@ function rellenarCarton() {
     carton.push(fila);
   }
 
-  return carton;
+  mostrarCarton();
 }
 
 function comprobarLinea(carton, numerosSalidos) {
-  let resultado;
+  let resultado = -1;
 
   for (let i = 0; i < carton.length; i++) {
     let lineaCompleta = true;
     for (let j = 0; j < carton[i].length; j++) {
-      if (!numerosSalidos.includes([i][j])) {
+      if (!numerosSalidos.includes(carton[i][j])) {
         lineaCompleta = false;
-        resultado = -1;
         break;
       }
     }
@@ -44,28 +46,58 @@ function comprobarLinea(carton, numerosSalidos) {
 }
 
 function comprobarBingo(carton, numerosSalidos) {
-  let resultado = false;
+  let resultado = true;
 
   for (let i = 0; i < carton.length; i++) {
-    let lineaCompleta = true;
     for (let j = 0; j < carton[i].length; j++) {
-      if (!numerosSalidos.includes([i][j])) {
+      if (!numerosSalidos.includes(carton[i][j])) {
+        resultado = false;
         break;
-      } else {
-        resultado = true;
       }
     }
   }
   return resultado;
 }
 
-function sacaNumeroNuevo(numeros) {
-  if (numSalidos < 99) {
+function sacaNumeroNuevo() {
+  if (numSalidos.length < 99) {
     let newNumero;
     do {
       newNumero = Math.floor(Math.random() * 99) + 1;
     } while (numSalidos.includes(newNumero));
     numSalidos.push(newNumero);
+    mostrarNumerosSalidos();
+    mostrarResultado();
   } else {
+    document.write("No se pueden sacar más números. ¡Fin del juego!");
+  }
+}
+
+function mostrarCarton() {
+  document.write('<div id="carton">');
+  for (let i = 0; i < carton.length; i++) {
+    document.write(`Fila ${i + 1}: ${carton[i].join(" | ")}<br>`);
+  }
+  document.write("</div>");
+}
+
+function mostrarNumerosSalidos() {
+  document.write('<div id="numeros-salidos">');
+  document.write(`Números que han salido: ${numSalidos.join(", ")}`);
+  document.write("</div>");
+}
+
+function mostrarResultado() {
+  let lineaCantada = comprobarLinea(carton, numSalidos);
+  let bingoCantado = comprobarBingo(carton, numSalidos);
+
+  if (bingoCantado) {
+    document.write('<div id="resultado">¡BINGO!</div>');
+  } else if (lineaCantada !== -1) {
+    document.write(`<div id="resultado">Línea ${lineaCantada} cantada</div>`);
+  } else {
+    document.write(
+      '<div id="resultado">No ha obtenido ningún resultado aún</div>'
+    );
   }
 }

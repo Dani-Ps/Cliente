ef4376c678b744c008088c8813f687caf7f12f20
Dani-Ps <daniperez439@gmail.com>
2023-11-13 13:41:05 +0100

    añadiendo tareas ud.6

---
+++ 1ºTrimestre/Tareas/UD.6/UD6T1/js/UD6T1_dps.js
@@ -0,0 +1,23 @@
+let text = document.getElementById("text");
+console.log(text);
+
+function colorChangeRed() {
+    let text = document.getElementById("text");
+    console.log(text);
+
+    text.style.color="red";
+    
+}
+
+function colorChangeYellow() {
+    let text = document.getElementById("text");
+    text.style.color="yellow";
+    
+}
+
+function colorChangeGreen() {
+    let text = document.getElementById("text");
+    
+    text.style.color="green";
+    
+}
---
+++ 1ºTrimestre/Tareas/UD.6/UD6T2/js/UD6T2_dps.js
@@ -0,0 +1,4 @@
+
+let numParrafos = document.getElementsByTagName("p");
+console.log(numParrafos.length);
+

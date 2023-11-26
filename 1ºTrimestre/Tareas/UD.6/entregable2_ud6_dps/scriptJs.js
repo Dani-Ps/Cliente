94bc1f854a51daf4f5f1b1c4ab78a5c272d1af8a
Daniel Perez Serrano <daniperez439@gmail.com>
2023-11-26 23:23:16 +0100

    'c'

--- 1ºTrimestre/Tareas/UD.6/entregable2_ud6_dps/scriptJs.js
+++ 1ºTrimestre/Tareas/UD.6/entregable2_ud6_dps/scriptJs.js
@@ -1,30 +1,24 @@
-// scriptJs.js
+// VARIABLES
+var numElemt = document.getElementById("numElementos");
+var camposTexto = document.getElementById("camposTexto");
+var lista = document.getElementById("lista");
 
-let numElemt = document.getElementById("numElementos");
-let camposTexto = document.getElementById("camposTexto");
-let lista = document.getElementById("lista");
-
-function addLi() {
-  // Obtener el número de elementos ingresado por el usuario
-  let numElementos = parseInt(numElemt.value);
-
-  // Limpiar el contenido previo en camposTexto
-  camposTexto.innerHTML = "";
-
-  // Crear campos de texto según el número ingresado
-  for (let i = 1; i <= numElementos; i++) {
+function addElementos() {
+  // Obtengo el numero de elementos
+  var numElementos = parseInt(numElemt.value);
+  // Creo los campos
+  for (var i = 0; i < numElementos; i++) {
     let inputText = document.createElement("input");
     inputText.type = "text";
-    inputText.placeholder = "Texto del LI " + i;
+    inputText.placeholder = "Introduce el texto del li " + i;
     camposTexto.appendChild(inputText);
   }
 }
 
-function agregarElementos() {
-  // Obtener todos los campos de texto creados
-  let campos = camposTexto.querySelectorAll("input[type='text']");
-
-  // Crear elementos LI y agregarlos a la lista
+function addLi() {
+  // Obtengo todos los campos de texto creados
+  var campos = camposTexto.querySelectorAll("input[type='text']");
+  // Creo elementos li y los agrego a la lista
   campos.forEach((campo) => {
     let li = document.createElement("li");
     li.textContent = campo.value;
@@ -33,7 +27,7 @@ function agregarElementos() {
 }
 
 function borrarPrimerLi() {
-  // Obtener el primer elemento de la lista y eliminarlo
+  // Obtengo el primer elemento de la lista y lo elimino
   let primerLi = lista.firstElementChild;
   if (primerLi) {
     lista.removeChild(primerLi);
@@ -41,23 +35,25 @@ function borrarPrimerLi() {
 }
 
 function borrarUltimoLi() {
-  // Obtener el último elemento de la lista y eliminarlo
+  // Obtener el último elemento de la lista y lo elimino
   let ultimoLi = lista.lastElementChild;
   if (ultimoLi) {
     lista.removeChild(ultimoLi);
   }
 }
 
-// Agregar eventos a los botones
+// Agrego los eventos a los botones
 document
   .querySelector("button:nth-of-type(1)")
-  .addEventListener("click", addLi);
+  .addEventListener("click", addElementos);
 document
   .querySelector("button:nth-of-type(2)")
-  .addEventListener("click", agregarElementos);
+  .addEventListener("click", addLi);
 document
   .querySelector("button:nth-of-type(3)")
   .addEventListener("click", borrarPrimerLi);
 document
   .querySelector("button:nth-of-type(4)")
   .addEventListener("click", borrarUltimoLi);
+
+// Daniel Perez Serrano

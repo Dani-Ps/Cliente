ed903816e2b966424c6207be5a7cc00d6924148b
Daniel Perez Serrano <daniperez439@gmail.com>
2023-12-04 09:43:45 +0100

    x

--- 1ºTrimestre/Tareas/UD.6/entregable_final/puebas.js
+++ 1ºTrimestre/Tareas/UD.6/entregable_final/puebas.js
@@ -1,155 +1,211 @@
-/**
- * Este script es el del juego del Bingo, tendrá 2 botones:
- * El primero de nueva partida que cargará el cartón con todos los números.
- * El Sergundo para sacar un nuevo número
- * Si el numero que sales es igual al alguno del carton, este cambia su fondo
- * Si se commpleta una linea aparece una animación índicandolo.
- * Si se completa el cartón se aparece una animación de bingo.
- */
+    document.addEventListener('DOMContentLoaded', function () {
+        // Crear elementos HTML
+        const loaderDiv = document.createElement('div');
+        loaderDiv.classList.add('loader');
+    
+        const numerosDiv = document.createElement('div');
+        numerosDiv.classList.add('numeros');
+    
+        const cartonDiv = document.createElement('div');
+        cartonDiv.classList.add('carton');
+    
+        const buttonsDiv = document.createElement('div');
+        const botonEmpezar = document.createElement('button');
+        botonEmpezar.id = 'begin';
+        botonEmpezar.textContent = 'Empezar partida';
+    
+        const botonNewNumero = document.createElement('button');
+        botonNewNumero.id = 'newNumber';
+        botonNewNumero.textContent = 'Sacar número';
+    
+        // Agregar elementos al documento
+        document.body.appendChild(loaderDiv);
+        loaderDiv.appendChild(numerosDiv);
+        loaderDiv.appendChild(cartonDiv);
+        loaderDiv.appendChild(buttonsDiv);
+    
+        // Función para mostrar y luego ocultar los elementos después de 4 segundos
+        setTimeout(function () {
+            // Ocultar el contenedor de loader después de 4 segundos
+            loaderDiv.style.display = 'none';
+    
+            // Agregar más elementos al documento después de 4 segundos
+            buttonsDiv.appendChild(botonEmpezar);
+            buttonsDiv.appendChild(botonNewNumero);
+    
+            console.log("Botones agregados:", botonEmpezar, botonNewNumero);
+    
+            // Asociar el evento de inicio con la función principal
+            botonEmpezar.addEventListener('click', function () {
+                comenzarJuego();
+            });
+    
+            botonNewNumero.addEventListener('click', function () {
+                mostrarNumero(numerosBingo, n);
+    
+                // Verificar si hay línea cantada después de cada número
+                if (filaCompletaCantada(carton.carton[0]) || filaCompletaCantada(carton.carton[1]) || filaCompletaCantada(carton.carton[2])) {
+                    lineasCantadas++;
+    
+                    // Si las tres líneas están cantadas, mostrar mensaje de bingo
+                    if (lineasCantadas === 3) {
+                        mostrarMensajeBingo();
+                    }
+                }
+            });
+    
+        }, 4000);
 
-// Constantes
-const cartonDiv = document.querySelector('.carton');
-const botonEmpezar = document.getElementById('begin');
-const botonNewNumero = document.getElementById('newNumber');
-const numerosDiv = document.querySelector('.numeros');
+    // Constantes y Variables Globales
+    const cartonDivElement = document.querySelector('.carton');
+    const botonEmpezarElement = document.getElementById('begin');
+    const botonNewNumeroElement = document.getElementById('newNumber');
+    const numerosDivElement = document.querySelector('.numeros');
 
-class Carton {
-    constructor() {
-        this.carton = [[], [], []];
-    }
-}
-var carton = new Carton();
-
-// Funcion para crear un número aleatorio entre 1 y 100
-function crearNumAleatorio() {
-    let numero = Math.floor(Math.random() * 100) + 1;
-    return numero;
-}
-
-// Funcion para generar numeros del carton
-function numCarton() {
-
-    let numeros = []; // Almacena los 15 numeros del carton
-    // Introduces numeros aleatorios entre el 1 y el 100.
-    do {
-        let random = crearNumAleatorio();
-        if (!numeros.includes(random)) {// Comprueba que no este en la lista 
-            numeros.push(random);
+    class Carton {
+        constructor() {
+            this.carton = [[], [], []];
         }
-    } while (numeros.length < 15); // Termina cuando array tiene 15 numeros
-    console.log('numerosCarton:', numeros);
-
-    return numeros; // Devuelve el array con todos los valores
-}
-
-// Funcion para generar 100 numeros aleatorios
-function numBingo() {
-    let numeros = []; // Almacena los 100 numeros del bingo
-    // Introduces numeros aleatorios entre el 1 y el 100.
-    do {
-        let random = crearNumAleatorio();
-        if (!numeros.includes(random)) { // Comprueba que no este en la lista 
-            numeros.push(random);
-        }
-    } while (numeros.length < 99); // Termina cuando array tiene 100 numeros
-    console.log('numerosBingo:', numeros);
-
-    return numeros; // Devuelve el array con todos los valores
-}
-
-// Funcion para rellenar un carton
-function rellenarCarton(numerosCarton) {
-    let nuevoCarton = [[], [], []];
-    for (let i = 0; i < nuevoCarton.length; i++) {
-        // Introduzco de 5 en 5 en cada fila
-        nuevoCarton[i] = numerosCarton.slice(0, 5);
-        // Borro los 5 primeros para que no se repita en la proxima iteracion
-        numerosCarton.splice(0, 5);
     }
-    return nuevoCarton;
-}
 
-// Funcion para mostrar el carton
-function mostrarCarton(carton) {
-    cartonDiv.innerHTML = ''; // Limpio el contenido anterior
+    // Funciones Auxiliares
+    function generarNumeroAleatorio() {
+        return Math.floor(Math.random() * 100) + 1;
+    }
 
-    carton.forEach(fila => {
-        const filaDiv = document.createElement('div');
-        filaDiv.classList.add('fila');
+    function generarNumerosCarton() {
+        let numeros = [];
+        do {
+            let random = generarNumeroAleatorio();
+            if (!numeros.includes(random)) {
+                numeros.push(random);
+            }
+        } while (numeros.length < 15);
+        console.log('numerosCarton:', numeros);
+        return numeros;
+    }
 
-        fila.forEach(numero => {
-            const icono = document.createElement('i');
+    function generarNumerosBingo() {
+        let numeros = [];
+        do {
+            let random = generarNumeroAleatorio();
+            if (!numeros.includes(random)) {
+                numeros.push(random);
+            }
+        } while (numeros.length < 99);
+        console.log('numerosBingo:', numeros);
+        return numeros;
+    }
+
+    function filaCompletaCantada(fila) {
+        return fila.every(icono => icono && icono.style && icono.style.color === 'blue');
+    }
+
+    function rellenarCarton(numerosCarton) {
+        let nuevoCarton = [[], [], []];
+        for (let i = 0; i < nuevoCarton.length; i++) {
+            nuevoCarton[i] = numerosCarton.slice(0, 5);
+            numerosCarton.splice(0, 5);
+        }
+        return nuevoCarton;
+    }
+
+    function mostrarCarton(carton) {
+        cartonDiv.innerHTML = '';
+        carton.forEach(fila => {
+            const filaDiv = document.createElement('div');
+            filaDiv.classList.add('fila');
+            fila.forEach(numero => {
+                const icono = document.createElement('i');
+                icono.classList.add('fa-regular', 'fa-circle', 'fa-2xl', 'fa-stack');
+                const numeroSpan = document.createElement('span');
+                numeroSpan.classList.add('fa-stack-1x');
+                numeroSpan.style.left = '-23px';
+                numeroSpan.textContent = numero;
+                icono.appendChild(numeroSpan);
+                numeroSpan.style.fontSize = '30%';
+                filaDiv.appendChild(icono);
+            });
+            cartonDiv.appendChild(filaDiv);
+        });
+    }
+
+    function mostrarNumero(numerosBingo, n) {
+        if (numerosBingo.length > 0) {
+            let icono = document.createElement('i');
             icono.classList.add('fa-regular', 'fa-circle', 'fa-2xl', 'fa-stack');
-
             const numeroSpan = document.createElement('span');
             numeroSpan.classList.add('fa-stack-1x');
             numeroSpan.style.left = '-23px';
-            numeroSpan.textContent = numero;
+            numeroSpan.textContent = numerosBingo[0];
+
+            if (n.includes(numerosBingo[0])) {
+                icono.style.color = 'green';
+                console.log('Color cambiado a verde');
+                const cartonIconos = document.querySelectorAll('.fa-stack span');
+                cartonIconos.forEach(cartonSpan => {
+                    if (cartonSpan.textContent === numerosBingo[0].toString()) {
+                        cartonSpan.parentNode.style.color = 'green';
+                    }
+                });
+                const filasCarton = document.querySelectorAll('.fila');
+                filasCarton.forEach(fila => {
+                    const iconosFila = fila.querySelectorAll('.fa-stack span');
+                    const todosVerdes = Array.from(iconosFila).every(span => span.parentNode.style.color === 'green');
+                    if (todosVerdes) {
+                        // Animación de línea cantada
+                        fila.style.animation = 'lineaCantada 1s ease-in-out';
+                        iconosFila.forEach(span => span.parentNode.style.color = 'blue');
+                    }
+                });
+            }
 
             icono.appendChild(numeroSpan);
             numeroSpan.style.fontSize = '30%';
-
-            filaDiv.appendChild(icono);
-        });
-
-        cartonDiv.appendChild(filaDiv);
-    });
-}
-// Funcion para sacar un número del bingo
-function MostrarNumero(numerosBingo, n) {
-    if (numerosBingo.length > 0) {
-        let icono = document.createElement('i');
-        icono.classList.add('fa-regular', 'fa-circle', 'fa-2xl', 'fa-stack');
-
-        const numeroSpan = document.createElement('span');
-        numeroSpan.classList.add('fa-stack-1x');
-        numeroSpan.style.left = '-23px';
-        numeroSpan.textContent = numerosBingo[0];
-
-        if (n.includes(numerosBingo[0])) {
-            icono.style.color = 'green';
-            console.log('Color cambiado a verde');
-
-            // Cambiar el color de los iconos en el cartón
-            const cartonIconos = document.querySelectorAll('.fa-stack span');
-            cartonIconos.forEach(cartonSpan => {
-                if (cartonSpan.textContent === numerosBingo[0].toString()) {
-                    cartonSpan.parentNode.style.color = 'green';
-                }
-            });
+            numerosDiv.appendChild(icono);
+            console.log('n:', n);
+            console.log('numerosBingo[0]:', numerosBingo[0]);
+            numerosBingo.shift();
         }
-
-        icono.appendChild(numeroSpan);
-        numeroSpan.style.fontSize = '30%';
-
-        numerosDiv.appendChild(icono);
-        console.log('n:', n);
-        console.log('numerosBingo[0]:', numerosBingo[0]);
-
-        numerosBingo.shift();
     }
-}
 
-// Funcion empezar
-function begin() {
-    // Rellenar el cartón con números aleatorios
-    carton = new Carton();
+    // Función principal para iniciar el juego
+    function comenzarJuego() {
+        // Rellenar el cartón con números aleatorios
+        let carton = new Carton();
+        const numerosDelCarton = generarNumerosCarton();
+        console.log('numerosDelCarton:', numerosDelCarton);
+        const numerosBingo = generarNumerosBingo();
+        var n = numerosDelCarton.slice();
+        carton.carton = rellenarCarton(numerosDelCarton);
+        mostrarCarton(carton.carton);
+        let lineasCantadas = 0;
 
-    const numerosDelCarton = numCarton();
-    console.log('numerosDelCarton:', numerosDelCarton);
-    const numerosBingo = numBingo();
-    var n = numerosDelCarton.slice();
-    carton.carton = rellenarCarton(numerosDelCarton);  // Usar el nuevo nombre
+        // Manejar eventos
+        botonNewNumero.addEventListener('click', function () {
+            mostrarNumero(numerosBingo, n);
 
-    // Mostrar el cartón en el DOM
-    mostrarCarton(carton.carton);
+            // Verificar si hay línea cantada después de cada número
+            if (filaCompletaCantada(carton.carton[0]) || filaCompletaCantada(carton.carton[1]) || filaCompletaCantada(carton.carton[2])) {
+                lineasCantadas++;
 
-    botonNewNumero.addEventListener('click', function () {
-        MostrarNumero(numerosBingo, n);  // Usar el nuevo nombre
+                // Si las tres líneas están cantadas, mostrar mensaje de bingo
+                if (lineasCantadas === 3) {
+                    mostrarMensajeBingo();
+                }
+            }
+        });
+    }
+
+    function mostrarMensajeBingo() {
+        const mensajeBingo = document.createElement('div');
+        mensajeBingo.textContent = '¡BINGO! Todas las líneas han sido cantadas.';
+        mensajeBingo.classList.add('bingo-message');
+        document.body.appendChild(mensajeBingo);
+    }
+
+    // Asociar el evento de inicio con la función principal
+    botonEmpezarElement.addEventListener('click', function () {
+        comenzarJuego();
     });
-}
-
-
-botonEmpezar.addEventListener('click', function () {
-    begin();
 });
--- 1ºTrimestre/Tareas/UD.6/entregable_final/script.js
+++ 1ºTrimestre/Tareas/UD.6/entregable_final/script.js
@@ -1,13 +1,4 @@
-/**
- * Este script es el del juego del Bingo, tendrá 2 botones:
- * El primero de nueva partida que cargará el cartón con todos los números.
- * El Sergundo para sacar un nuevo número
- * Si el numero que sales es igual al alguno del carton, este cambia su fondo
- * Si se commpleta una linea aparece una animación índicandolo.
- * Si se completa el cartón se aparece una animación de bingo.
- */
-
-// Constantes
+// Constantes y Variables Globales
 const cartonDiv = document.querySelector('.carton');
 const botonEmpezar = document.getElementById('begin');
 const botonNewNumero = document.getElementById('newNumber');
@@ -18,89 +9,73 @@ class Carton {
         this.carton = [[], [], []];
     }
 }
-var carton = new Carton();
 
-// Funcion para crear un número aleatorio entre 1 y 100
-function crearNumAleatorio() {
-    let numero = Math.floor(Math.random() * 100) + 1;
-    return numero;
+// Funciones Auxiliares
+function generarNumeroAleatorio() {
+    return Math.floor(Math.random() * 100) + 1;
 }
 
-// Funcion para generar numeros del carton
-function numCarton() {
-
-    let numeros = []; // Almacena los 15 numeros del carton
-    // Introduces numeros aleatorios entre el 1 y el 100.
+function generarNumerosCarton() {
+    let numeros = [];
     do {
-        let random = crearNumAleatorio();
-        if (!numeros.includes(random)) {// Comprueba que no este en la lista 
+        let random = generarNumeroAleatorio();
+        if (!numeros.includes(random)) {
             numeros.push(random);
         }
-    } while (numeros.length < 15); // Termina cuando array tiene 15 numeros
+    } while (numeros.length < 15);
     console.log('numerosCarton:', numeros);
-
-    return numeros; // Devuelve el array con todos los valores
+    return numeros;
 }
 
-// Funcion para generar 100 numeros aleatorios
-function numBingo() {
-    let numeros = []; // Almacena los 100 numeros del bingo
-    // Introduces numeros aleatorios entre el 1 y el 100.
+function generarNumerosBingo() {
+    let numeros = [];
     do {
-        let random = crearNumAleatorio();
-        if (!numeros.includes(random)) { // Comprueba que no este en la lista 
+        let random = generarNumeroAleatorio();
+        if (!numeros.includes(random)) {
             numeros.push(random);
         }
-    } while (numeros.length < 99); // Termina cuando array tiene 100 numeros
+    } while (numeros.length < 99);
     console.log('numerosBingo:', numeros);
-
-    return numeros; // Devuelve el array con todos los valores
+    return numeros;
 }
 
-// Funcion para rellenar un carton
+function filaCompletaVerde(fila) {
+    return fila.every(icono => icono.style.color === 'green');
+}
+
 function rellenarCarton(numerosCarton) {
     let nuevoCarton = [[], [], []];
     for (let i = 0; i < nuevoCarton.length; i++) {
-        // Introduzco de 5 en 5 en cada fila
         nuevoCarton[i] = numerosCarton.slice(0, 5);
-        // Borro los 5 primeros para que no se repita en la proxima iteracion
         numerosCarton.splice(0, 5);
     }
     return nuevoCarton;
 }
 
-// Funcion para mostrar el carton
 function mostrarCarton(carton) {
-    cartonDiv.innerHTML = ''; // Limpio el contenido anterior
-
+    cartonDiv.innerHTML = '';
     carton.forEach(fila => {
         const filaDiv = document.createElement('div');
         filaDiv.classList.add('fila');
-
         fila.forEach(numero => {
             const icono = document.createElement('i');
             icono.classList.add('fa-regular', 'fa-circle', 'fa-2xl', 'fa-stack');
-
             const numeroSpan = document.createElement('span');
             numeroSpan.classList.add('fa-stack-1x');
             numeroSpan.style.left = '-23px';
             numeroSpan.textContent = numero;
-
             icono.appendChild(numeroSpan);
             numeroSpan.style.fontSize = '30%';
-
             filaDiv.appendChild(icono);
         });
-
         cartonDiv.appendChild(filaDiv);
     });
 }
-// Funcion para sacar un número del bingo
-function MostrarNumero(numerosBingo, n) {
+
+function mostrarNumero(numerosBingo, n) {
     if (numerosBingo.length > 0) {
         let icono = document.createElement('i');
         icono.classList.add('fa-regular', 'fa-circle', 'fa-2xl', 'fa-stack');
-
         const numeroSpan = document.createElement('span');
         numeroSpan.classList.add('fa-stack-1x');
         numeroSpan.style.left = '-23px';
@@ -109,47 +84,51 @@ function MostrarNumero(numerosBingo, n) {
         if (n.includes(numerosBingo[0])) {
             icono.style.color = 'green';
             console.log('Color cambiado a verde');
-
-            // Cambiar el color de los iconos en el cartón
             const cartonIconos = document.querySelectorAll('.fa-stack span');
             cartonIconos.forEach(cartonSpan => {
                 if (cartonSpan.textContent === numerosBingo[0].toString()) {
                     cartonSpan.parentNode.style.color = 'green';
                 }
             });
+            const filasCarton = document.querySelectorAll('.fila');
+            filasCarton.forEach(fila => {
+                const iconosFila = fila.querySelectorAll('.fa-stack span');
+                const todosVerdes = Array.from(iconosFila).every(span => span.parentNode.style.color === 'green');
+                if (todosVerdes) {
+                    // Animación de línea cantada
+                    fila.style.animation = 'lineaCantada 1s ease-in-out';
+                    iconosFila.forEach(span => span.parentNode.style.color = 'blue');
+                }
+            });
         }
 
         icono.appendChild(numeroSpan);
         numeroSpan.style.fontSize = '30%';
-
         numerosDiv.appendChild(icono);
         console.log('n:', n);
         console.log('numerosBingo[0]:', numerosBingo[0]);
-
         numerosBingo.shift();
     }
 }
 
-// Funcion empezar
-function begin() {
+// Función principal para iniciar el juego
+function comenzarJuego() {
     // Rellenar el cartón con números aleatorios
-    carton = new Carton();
-
-    const numerosDelCarton = numCarton();
+    let carton = new Carton();
+    const numerosDelCarton = generarNumerosCarton();
     console.log('numerosDelCarton:', numerosDelCarton);
-    const numerosBingo = numBingo();
+    const numerosBingo = generarNumerosBingo();
     var n = numerosDelCarton.slice();
-    carton.carton = rellenarCarton(numerosDelCarton);  // Usar el nuevo nombre
-
-    // Mostrar el cartón en el DOM
+    carton.carton = rellenarCarton(numerosDelCarton);
     mostrarCarton(carton.carton);
 
+    // Manejar eventos
     botonNewNumero.addEventListener('click', function () {
-        MostrarNumero(numerosBingo, n);  // Usar el nuevo nombre
+        mostrarNumero(numerosBingo, n);
     });
 }
 
-
+// Asociar el evento de inicio con la función principal
 botonEmpezar.addEventListener('click', function () {
-    begin();
+    comenzarJuego();
 });

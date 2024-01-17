a140dab8276b592b8d57ab15d2d3ef01074cf20d
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-01-17 19:16:10 +0100

    done t3

--- 1ºTrimestre/Tareas/UD.5/U5T3_dps/js3.js
+++
@@ -1,7 +0,0 @@
-const clave = document.getElementById("clave");
-const valor = document.getElementById("valor");
-const botones = document.querySelectorAll('.miBoton');
-
-const comprobarCompatibilidad = document.getElementById("");
-
-

d47c199bd7cef0e9f89779f390af7a5fb4da874f
Daniel Perez Serrano <117571463+Dani-Ps@users.noreply.github.com>
2024-02-13 18:19:44 +0100

    t3 completed

--- 1ºTrimestre/Tareas/UD.7/UD07T3-dps/script.js
+++ 1ºTrimestre/Tareas/UD.7/UD07T3-dps/script.js
@@ -14,16 +14,28 @@ document.getElementById('formulario').addEventListener('submit', function (event
             return response.json();
         })
         .then(data => {
-            // Mostrar los datos en la página principal
-            document.getElementById('respuesta').innerHTML = `
-            <p>Nombre: ${data.nombre}</p>
-            <p>Apellido: ${data.apellido}</p>
-            <p>Foto:</p>
-            <img src="${data.ruta_imagen}" alt="Foto de alumno">
-        `;
+            var alumnoDiv = document.createElement('div');
+            alumnoDiv.classList.add('alumno');
+
+            var img = document.createElement('img');
+            img.src = data.ruta_imagen;
+            img.alt = 'Foto de alumno';
+            img.classList.add('rounded-circle', 'img-fluid');
+
+            var nombreApellido = document.createElement('p');
+            nombreApellido.textContent = `${data.nombre} ${data.apellido}`;
+            nombreApellido.classList.add('nombre-apellido');
+
+            alumnoDiv.appendChild(img);
+            alumnoDiv.appendChild(nombreApellido);
+
+            document.getElementById('grid-alumnos').appendChild(alumnoDiv);
         })
         .catch(error => {
             console.error('Error:', error);
             document.getElementById('respuesta').innerHTML = 'Error al procesar la solicitud';
         });
 });
+/**
+ * Daniel Perez Serrano
+ */
